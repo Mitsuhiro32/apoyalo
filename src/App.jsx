536a68a2fadb51fa909f3f82c1af358c5e453f1d
Mitsuhiro32 <mitsueiwa333@gmail.com>
2024-05-08 16:21:31 -0400

    Actividad "Poniendo todo junto"

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import PersonCard from './components/PersonCard'
 function App() {
   return (
     <>
-      <PersonCard firstName={"Doe"} lastName={"Jane"} age={45} hairColor={"Black"} />
-      <PersonCard firstName={"Smith"} lastName={"John"} age={88} hairColor={"Brown"} />
-      <PersonCard firstName={"Fillmore"} lastName={"Millard"} age={50} hairColor={"Brown"} />
-      <PersonCard firstName={"Smith"} lastName={"Maria"} age={62} hairColor={"Brown"} />
+      <PersonCard firstName={"Jane"} lastName={"Doe"}  age={45} hairColor={"Black"} />
+      <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"} />
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"} />
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"} />
     </>
   )
 }
--- src/components/PersonCard/index.jsx
+++ src/components/PersonCard/index.jsx
@@ -2,12 +2,26 @@ import { Component } from "react";
 import PropTypes from "prop-types";
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    birthdayButton = () => {
+        this.setState({
+            age: this.state.age + 1
+        });
+    }
+
     render() {
         return (
             <div className="person-card">
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.birthdayButton}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         );
     }
